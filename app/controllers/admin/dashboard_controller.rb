45bc68cf51912ae6468f1b0f16c37be5aa746f44
Karen Mui <karen_mui@hotmail.com>
2023-01-30 02:43:40 +0000

    added information to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => "Jungle", :password => "book" 
 
   def show
+    @product_quantity = Product.count
+    @category_amount = Category.count
   end
 
 end
